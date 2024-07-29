271b6c9a77edc19429ebe5361dcb9635d7b0fd2b
karishma244 <karishmajethwani4@gmail.com>
2024-07-29 21:55:53 +0530

    final code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings: ["","Web Developer", "Mern Developer", "Java Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
